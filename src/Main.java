af1480fbbfb2f60d597dafdc9bdccba19f2bf5b3
Rafael <rafaelleonardodasilva.dv@gmail.com>
2024-11-29 18:31:26 -0300

    tirando linha de teste

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         MenuView menu = new MenuView();
         menu.setVisible(true);
-        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
