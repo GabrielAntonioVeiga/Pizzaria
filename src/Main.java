f2c4c0e63b0ea78f885fa4680e85923d51e1c1b8
Rafael <rafaelleonardodasilva.dv@gmail.com>
2024-11-29 18:28:59 -0300

    testando push branch RAFAEL

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         MenuView menu = new MenuView();
         menu.setVisible(true);
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
