e9b24ed8aa7947908da22a3b4b18d6ebc16c4073
Rafael <rafaelleonardodasilva.dv@gmail.com>
2025-06-24 23:12:06 -0300

    refact: refatora pedidos

--- src/controller/PedidosController.java
+++ src/controller/PedidosController.java
@@ -7,6 +7,7 @@ import factory.DAOFactory;
 import model.Cliente;
 import model.Pedido;
 import model.Pizza;
+import view.ItensPedidoFormView;
 import view.PedidosView;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 public class PedidosController {
     private IPedidoDao pedidoDao = DAOFactory.getPedidoDao();
     private IPizzaDao pizzaDao = DAOFactory.getPizzaDao();
+    private ClienteController clienteController = new ClienteController();
     private PedidosView view;
 
     public PedidosController(PedidosView view) {
@@ -38,8 +40,12 @@ public class PedidosController {
 
     public void alterarStatusPedido(Long idPedido, EnStatusPedido novoStatus) {
         Pedido pedido = retornarPedidoPeloId(idPedido);
+
+        view.setStatusPedidoField(pedido.getStatus().toString());
+
         pedidoDao.alterarStatusPedido(idPedido, novoStatus);
         pedido.setStatus(novoStatus);
+        view.renderizarItensNaTabela(carregarPedidos());
     }
 
     public void alterarPrecoPedido(Long idPedido, double preco) {
@@ -52,12 +58,41 @@ public class PedidosController {
         return pedido;
     }
 
+    public void adicionarPedido(String telefone) {
+        Cliente cliente = clienteController.buscarClientePorTelefone(telefone);
+        if (cliente == null) {
+            view.exibirMensagemErro("É necessário buscar um cliente para adicionar um pedido!");
+            view.limparTabela();
+            return;
+        }
+        Long idPedido = clienteController.criarPedidoCliente(cliente.getId());
+        new ItensPedidoFormView(idPedido);
+    }
 
     public void deletarPedido(Long idPedido) {
         pedidoDao.remover(idPedido);
+        view.exibirMensagemSucesso("Sucesso", "Pedido removido com sucesso");
+        view.renderizarItensNaTabela(carregarPedidos());
     }
 
+    public void buscarPedidosClientePorNumero(String numero) {
+        Cliente clienteEncontrado = clienteController.buscarClientePorTelefone(numero);
 
+        if(clienteEncontrado == null) {
+            view.exibirMensagemErro("Cliente com o número digitado não encontrado!");
+            return;
+        }
 
+        List<Pedido> pedidos = carregarPedidosPorCliente(clienteEncontrado);
+        view.renderizarItensNaTabela(pedidos);
+
+        view.exibirMensagemSucesso("Busca","Cliente encontrado!");
+        view.habilitarAlterarStatusPedido();
+
+        if(pedidos.isEmpty()) {
+            view.exibirMensagemInfo("Cliente sem pedidos.","O cliente encontrado não possui pedidos, adicione um pedido!");
+        }
+
+    }
 
 }
